7ea2aff9cca2f7563a1801bbb8d50ca631bef925
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-05-23 14:09:27 -0400

    Agregando el ejercicio 3 Capitulo 10

---
+++ Ejercicios/Capitulo 10/Polígono.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios.Capitulo_10
+{
+    class Polígono
+    {
+
+        public int Lados { get; set; }
+        public int Vertices { get; set; }
+        public float AnguloInterior { get; set; }
+        public float AnguloExterior { get; set; }
+
+        public Polígono()
+        {
+            this.Lados = 0;
+            this.Vertices = 0;
+            this.AnguloInterior = 0.0f;
+            this.AnguloExterior = 0.0f;
+        }
+
+        public Polígono(int lados,int vertices,float anguloInterior,float anguloExterioro)
+        {
+            this.Lados = lados;
+            this.Vertices = vertices;
+            this.AnguloInterior = anguloInterior;
+            this.AnguloExterior = anguloExterioro;
+        }
+    }
+}
