03b33caebff8ec6686411d035ca9f08dd1976a84
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-05-23 11:31:05 -0400

    Agregando el Ejercicio 3 capitulo 9

--- Ejercicios/Capitulo 9/Capitulo9.cs
+++ Ejercicios/Capitulo 9/Capitulo9.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,7 @@ namespace Ejercicios.Capitulo_9
 {
     public partial class Capitulo9 : Form
     {
+        List<Mascota> lista;
         public struct Producto
         {
             public string nombre;
@@ -19,14 +21,39 @@ namespace Ejercicios.Capitulo_9
             public DateTime fechaCaducidad;
         }
 
+        public struct Dueño
+        {
+            public string nombre;
+            public string direccion;
+            public string telefono;
+
+            public Dueño(string nombre,string direccion,string telefono)
+            {
+                this.nombre = nombre;
+                this.direccion = direccion;
+                this.telefono = telefono;
+            }
+        }
+
+        public struct Mascota
+        {
+            public string nombre;
+            public int edad;
+            public string raza;
+            public Dueño dueño;
+        }
+
+
+
         public Capitulo9()
         {
             InitializeComponent();
+            lista = new List<Mascota>();
         }
 
         private void AgregarProductobutton_Click(object sender, EventArgs e)
         {
-
+            //boton de agregar del ejercicio 1 del capitulo 9
             Producto producto;
             producto.nombre =NombreProductotextBox.Text;
             producto.precio = (float)PrecioProductonumericUpDown.Value;
@@ -40,6 +67,62 @@ namespace Ejercicios.Capitulo_9
             NombreProductotextBox.Text = string.Empty;
             PrecioProductonumericUpDown.Value = 0;
             FechaCaducidaddateTimePicker.Value = DateTime.Now;
+        }
+
+        private void GuardarMascotabutton_Click(object sender, EventArgs e)
+        {
+            Mascota mascota;
+
+            mascota.nombre = NombreMascotatextBox.Text;
+            mascota.edad = (int)EdadMascotanumericUpDown.Value;
+            mascota.raza = RazaMascotatextBox.Text;
+            mascota.dueño.nombre = NombreDueñotextBox.Text;
+            mascota.dueño.telefono = TelefonoDueñotextBox.Text;
+            mascota.dueño.direccion = DirecciónDueñotextBox.Text;
+
+            lista.Add(mascota);
+            LimpiarMascota();
+        }
+
+        private void LimpiarMascota()
+        {
+            NombreMascotatextBox.Text = string.Empty; ;
+            EdadMascotanumericUpDown.Value=0;
+            RazaMascotatextBox.Text = string.Empty; ;
+            NombreDueñotextBox.Text = string.Empty; ;
+            TelefonoDueñotextBox.Text = string.Empty; ;
+            DirecciónDueñotextBox.Text = string.Empty; ;
+        }
+
+        private void Nuevobutton_Click(object sender, EventArgs e)
+        {
+            LimpiarMascota();
+        }
+
+        private void BuscarMascotabutton_Click(object sender, EventArgs e)
+        {
+
+
+                Mascota mascota = new Mascota() ;
+                foreach (Mascota obj in lista)
+                {
+                    if(obj.nombre.Equals(NombreMascotatextBox.Text))
+                    {
+                        mascota = obj;
+                        break;
+                    }
+                        
+                }
+                
+
+                NombreMascotatextBox.Text = mascota.nombre;
+                EdadMascotanumericUpDown.Value = mascota.edad;
+                RazaMascotatextBox.Text = mascota.raza;
+                NombreDueñotextBox.Text = mascota.dueño.nombre;
+                TelefonoDueñotextBox.Text = mascota.dueño.telefono;
+                DirecciónDueñotextBox.Text = mascota.dueño.direccion;
+
+           
         }
     }
 }
